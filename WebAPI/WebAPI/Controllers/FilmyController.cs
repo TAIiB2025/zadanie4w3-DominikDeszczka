db7fb3a3737f2983d4d1be5a3f24668ece97b00d
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2025-06-12 16:17:55 +0200

    Add files via upload

---
+++ WebAPI/WebAPI/Controllers/FilmyController.cs
@@ -0,0 +1,54 @@
+﻿using BLL;
+using BLL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmyController :ControllerBase
+    {
+        private readonly IFilmyService filmyService;
+   
+
+         public FilmyController(IFilmyService filmyService)
+        {
+            this.filmyService = filmyService;
+        }
+
+        //serwer/api/filmy
+        [HttpGet]
+        public IEnumerable<FilmyDTO> Get()
+        {
+            return filmyService.Get();
+        }
+
+        //serwer/api/filmy/4
+        [HttpGet("{id}")]
+        public FilmyDTO GetByID(int id)
+        {
+            return filmyService.GetById(id);
+        }
+
+        //server/api/filmy
+        [HttpPost]
+        public void Post([FromBody] FilmyPostDTO o)
+        {
+            filmyService.Post(o);
+        }
+
+        //server/api/filmy/4
+        [HttpPut("{id}")]
+        public void Put(int id, FilmyPostDTO film)
+        {
+            filmyService.Put(id, film);
+        }
+
+        //server/api/filmy/4
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            filmyService.Delete(id);
+        }
+    }
+}
--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -1,22 +1,32 @@
+using BLL;
+using BLL_Memory;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IFilmyService, FilmyService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors();
 
 app.UseAuthorization();
 
